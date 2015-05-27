56a724b42780d4a52e96e4fc55b47494425c57cc
MathieuGi <math.girardon@hotmail.fr>
2015-05-27 23:35:35 +0200

    rename
    rename

--- invariants/header.php
+++ invariants/header.php
@@ -1,61 +1,36 @@
-<!DOCTYPE html>
-
-<html>
-
-	<head>
-		<meta charset="utf-8" />
-		<link rel="stylesheet" type="text/css" href="../css/invariants.css">
-		
-
-	</head>
-
-	<body>
-		<<?php include ("body.php") ?>
-		<header id="Header">
-
-						<img id="Logo" src="logo_i_need_eat_miniature.png">
-
-			<div id="Recherche">
-
-					<h2> Vente et échange de fruits et légumes frais </h2>
-					    <input type="search" name="Recherche" id="Barre" placeholder="Fruits,Légumes..." size="40"/>
-						<input type="submit" value="Rechercher">
-
-			</div>
-
-			<section id="Connexion">
-				<div id="Identifiant">
-
-				<form method="post" action="#.php">
-					<p>
-						<label for="ID">Votre email:</label> 
-						<input type="Email" name="Email" id="Email" placeholder="exemple@ex.com" size="15"/>
-					<br>
-					<br>
-						<label for="M_d_P">Mot de passe:</label> 
-						<input type="password" name="M_D_P" id="M_D_P" placeholder="xxxxxxxx"  size="15"/>
-					<br>
-						<a href="formulaireMDP.php"> Mot de passe oublié?				</a>
-					</p>
-				</form>	
-				</div>	
-				
-
-				<div id="Bouton">
-
-						<a href="connect.php" class="myButton"> Connectez-vous!          </a>
-						
-					<br>
-						<a href="formulaire.php"> Pas encore Inscrit?					</a>
-				</div>
-			</section>
-			<br>
-			<br>
-			<nav id="navigation">
-				<table>
-					<td> <a href="Offres.html"> Offres 									</a>	</td>
-					<td> <a href="PoserAnnonces.html"> Proposer une annonce 					</a>	</td>
-					<td> <a href="MesAnnonces.html"> Mes Annonces 						</a>	</td>
-				</table>				
-			</nav>
-		</header>
+	<header>	
+		<article id="log">
+			<p>
+				<a href="../accueil/accueil.php">
+					<img src="../images/logo_i_need_eat_miniature.png" alt="logo I need Eat" title="page d'accueil" id="logo">
+				</a>
+			</p>
+		</article>
+		<div id="search_bar">	
+			<form action="../invariants/traitement.php" method="post" >
+				<p>
+					<input type="search" name="search" placeholder="recherche" size="50" maxlength="50"/>
+				</p>
+			</form>
+		</div>
+		<section>		
+			<?php
+			if(isset($_SESSION['pseudo']))
+			{
+				include("../invariants/connexion_connecte.php");
+			}
+			else
+			{
+				include("../invariants/connexion_non_connecte.php");
+			}
+			?>
+		</section>
+		<nav class="nav_header">
+			<ul class="menu">
+				<li><a href="../offres/offres.php">Offres</a></li>
+				<li><a href="#">Demandes</a></li>
+				<li><a href="../formulaires/annonce.php">Poser une annonce</a></li>
+				<li><a href="../compte/mes_annonces.php">Mes annonces</a></li>
+			</ul>
+		</nav>
+	</header>
