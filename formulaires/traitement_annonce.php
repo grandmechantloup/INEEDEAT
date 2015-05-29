e26b068da1a376748e2300d4cd10cfc32808273a
Gatty <simongatty3@gmail.com>
2015-05-29 14:59:33 +0200

    simon

--- formulaires/traitement_annonce.php
+++ formulaires/traitement_annonce.php
@@ -11,7 +11,7 @@ $_SESSION['pseudo'] = "joker";
 	<title> cible annonce </title>
 </head>
 
-<?php include("../bdd/connection.php"); ?>
+<?php include("../bdd/connexion.php"); ?>
 
 <?php
 if(isset($_FILES['image']['error']))
@@ -33,20 +33,13 @@ $extension_upload = strtolower(  substr(  strrchr($_FILES['image']['name'], '.')
 
 if($erreur=="aucune")
 {
-$req = $bdd->prepare('INSERT INTO categorie (categorie) VALUES (:categorie)');
-$req->execute(array(
-	
-	'categorie' => $_POST['categorie']));
 
-$req = $bdd->prepare('INSERT INTO variete (variete) VALUES (:variete)');
-$req->execute(array(
-	
-	'variete' => $_POST['variete']));
 
-$req = $bdd->prepare('INSERT INTO annonces (Prix, Quantite, Description, Auteur, Code_postal, Date_publication, Extension_upload) VALUES (:Prix, :Quantite, :Description, :Auteur, :Code_postal, NOW(), :Extension_upload)');
+$req = $bdd->prepare('INSERT INTO annonces (Prix, Quantite, Description, Auteur, Code_postal, Date_publication, Extension_upload) VALUES (:Prix, :Quantite, :Description, :Auteur, :Code_postal, NOW(), :Extension_upload)', 'INSERT INTO categorie (categorie) VALUES (:categorie)', 'INSERT INTO variete (variete) VALUES (:variete)');
 $req->execute(array(
 	
-    
+	'variete' => $_POST['variete'],
+    'categorie' => $_POST['categorie'],
     'Prix' => $_POST['prix'],
     'Quantite' => $_POST['quantite'],
     'Titre' => $_POST['titre'],
@@ -77,26 +70,59 @@ header('location:http://localhost/INEEDEAT/formulaires/annonce.php?erreur=' .$er
 
 <?php
 
-/*
-{
-$ImageNews = getimagesize($_FILES['image']['name']);
-                              if($ImageNews['mime'] == $ListeExtension[$ExtensionPresumee]  || $ImageNews['mime'] == $ListeExtensionIE[$ExtensionPresumee])
-{
-$ImageChoisie = imagecreatefromjpeg($_FILES['image']['name']);
-$TailleImageChoisie = getimagesize($_FILES['image']['name']);
-$NouvelleLargeur = 350; 
-$NouvelleHauteur = ( ($TailleImageChoisie[1] * (($NouvelleLargeur)/$TailleImageChoisie[0])) );
-$NouvelleImage = imagecreatetruecolor($NouvelleLargeur , $NouvelleHauteur) or die ("Erreur");
-imagecopyresampled($NouvelleImage , $ImageChoisie  , 0,0, 0,0, $NouvelleLargeur, $NouvelleHauteur, $TailleImageChoisie[0],$TailleImageChoisie[1]);
-imagedestroy($ImageChoisie);
-$NomImageChoisie = explode('.', $ImageNews);
-$NomImageExploitable = time();
-imagejpeg($NouvelleImage , 'image/'.$NomImageExploitable.'.'.$ExtensionPresumee, 100);
-$LienImageNews = 'image/'.$NomImageExploitable.'.'.$ExtensionPresumee;
- 
-                                             
-        }
-}*/
+$chemin  = "http://localhost/INEEDEAT/Image/{$name[0]}.{$extension_upload}";
+$x_c     = 640; // Taille de l'image
+$y_c     = 480;
+$qualite = 80; // Qualite de l'image (0=pourrit/100=super)
+$color   = "000000"; // Couleur de fond
+
+$size = getimagesize($chemin);
+
+if($size[0] >= $x_c AND $size[1] >= $y_c) {
+    if(($size[0]/$x_c) > ($size[1]/$y_c)) {
+        $x_t = $x_c;
+        $y_t = floor(($size[1]*$x_c)/$size[0]);
+        $x_p = 0;
+        $y_p = ($y_c/2)-($y_t/2);
+    } else {
+        $x_t = floor(($size[0]*$y_c)/$size[1]);
+        $y_t = $y_c;
+        $x_p = ($x_c/2)-($x_t/2);
+        $y_p = 0;
+    }
+} else {
+    $x_t = $size[0];
+    $y_t = $size[1];
+    $x_p = ($x_c/2)-($x_t/2);
+    $y_p = ($y_c/2)-($y_t/2);
+}
+
+$extension = strrchr($chemin,'.');
+$extension = strtolower(substr($extension,1));
+
+if($extension == 'jpg' OR $extension == 'jpeg') {
+    $image_new = imagecreatefromjpeg($chemin);
+} elseif($extension == 'gif') {
+    $image_new = imagecreatefromgif($chemin);
+} elseif($extension == 'png') {
+    $image_new = imagecreatefrompng($chemin);
+} elseif($extension == 'bmp') {
+    $image_new = imagecreatefromwbmp($chemin);
+} else {
+    echo "Erreur !";
+    exit;
+}
+
+Header("Content-type: image/jpeg");
+
+$image = imagecreatetruecolor($x_c, $y_c);
+$color = imagecolorallocate($image, hexdec($color[0].$color[1]), hexdec($color[2
+].$color[3]), hexdec($color[4].$color[5]));
+imagefilledrectangle($image,0,0,$x_c,$y_c,$color);
+imagecopyresampled($image,$image_new,$x_p,$y_p,0,0,$x_t,$y_t,$size[0],$size[1]);
+imagejpeg($image,NULL,$qualite);
+
+?>
 ?>
 
 
