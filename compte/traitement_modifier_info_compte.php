1b922b4d39d55119f550abf597a64ddd6c8f2813
MathieuGi <math.girardon@hotmail.fr>
2015-06-02 15:51:09 +0200

    suppression
    afezd

--- compte/traitement_modifier_info_compte.php
+++ compte/traitement_modifier_info_compte.php
@@ -4,15 +4,15 @@ $req=$bdd->prepare('SELECT Mdp FROM utilisateurs WHERE id_utilisateur=?');
 $req->execute(array($_SESSION['id_utilisateur']));
 $donnees=$req->fetch();
 if(isset($_POST['nouveau_pseudo']) AND $_POST['nouveau_pseudo']!=NULL)
-	{
-		$reponse1=$bdd->prepare('UPDATE utilisateurs SET Pseudo=:nouveau_pseudo WHERE id_utilisateur=:id_utilisateur');
-		$reponse1->execute(array(
-			'nouveau_pseudo'=>$_POST['nouveau_pseudo'],
-			'id_utilisateur'=>$_SESSION['id_utilisateur']
-		));
-		$reponse1->closeCursor();
-		$_SESSION['pseudo']=$_POST['nouveau_pseudo'];
-	}
+{
+	$reponse1=$bdd->prepare('UPDATE utilisateurs SET Pseudo=:nouveau_pseudo WHERE id_utilisateur=:id_utilisateur');
+	$reponse1->execute(array(
+		'nouveau_pseudo'=>$_POST['nouveau_pseudo'],
+		'id_utilisateur'=>$_SESSION['id_utilisateur']
+	));
+	$reponse1->closeCursor();
+	$_SESSION['pseudo']=$_POST['nouveau_pseudo'];
+}
 if(isset($_POST['ancien_mdp']) AND $_POST['ancien_mdp']==$donnees['Mdp'])
 {
 	if(isset($_POST['nouveau_mdp']) AND $_POST['nouveau_mdp']!=NULL AND $_POST['nouveau_mdp']===$_POST['confirmation_mdp'])
@@ -33,6 +33,6 @@ if(isset($_POST['nouvel_email']) AND $_POST['nouvel_email']!=NULL)
 			'id_utilisateur'=>$_SESSION['id_utilisateur']
 		));
 		$reponse1->closeCursor();
-	}
+}
 header('location:../compte/mon_compte.php');
 ?>
