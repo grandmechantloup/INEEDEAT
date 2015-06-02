b417dfda7b9f3b7c47766b709a5df4b4abc39ab3
MathieuGi <math.girardon@hotmail.fr>
2015-06-02 17:45:56 +0200

    nouvelle fonctionnalité
    panier disponible

--- compte/panier.php
+++ compte/panier.php
@@ -22,6 +22,9 @@ $req->execute(array($_SESSION['id_utilisateur']));
 while($donnees=$req->fetch())
 {
 	include("../annonces/produit_simple.php");
+	?>
+	<a href="../suppression/supprimer.php?annonce=<?php echo $donnees['id_annonce']?>"><input type="button" value="Supprimer"/></a>
+<?php
 }
 include("../invariants/footer.php");
 ?>
