6234d968692816cea607a12ad22996cad1372f7a
MathieuGi <math.girardon@hotmail.fr>
2015-06-02 14:53:51 +0200

    math
    zfscij
    
    sx
    
    s

--- invariants/header.php
+++ invariants/header.php
@@ -1,38 +1,38 @@
-	<header id="Header">	
-		<article id="log">
+<header id="Header">	
+	<article id="log">
+		<p>
+			<a href="../accueil/accueil.php">
+				<img src="../images/images_site/logo_i_need_eat_miniature.png" alt="logo I need Eat" title="page d'accueil" id="logo">
+			</a>
+		</p>
+	</article>
+	<div id="recherche">	
+		<h2> Vente et échange de fruits et légumes frais </h2>
+		<form action="" method="post" >
 			<p>
-				<a href="../accueil/accueil.php">
-					<img src="../images/logo_i_need_eat_miniature.png" alt="logo I need Eat" title="page d'accueil" id="logo">
-				</a>
+				<input type="search" name="recherche" id="Barre" placeholder="Fruits, légumes, ..." size="60" maxlength="50"/>
 			</p>
-		</article>
-		<div id="recherche">	
-			<h2> Vente et échange de fruits et légumes frais </h2>
-			<form action="" method="post" >
-				<p>
-					<input type="search" name="recherche" id="Barre" placeholder="Fruits, légumes, ..." size="60" maxlength="50"/>
-				</p>
-			</form>
-		</div>
-		<section>		
-			<?php
-			if(isset($_SESSION['pseudo']))
-			{
-				include("../invariants/connexion_connecte.php");
-			}
-			else
-			{
-				include("../invariants/connexion_non_connecte.php");
-			}
-			?>
-		</section>
-		<br/>
-		<br/>
-		<nav id="navigation">
-			<table>
-				<td><a href="../offres/offres.php">Offres</a></td>
-				<td><a href="../formulaires/annonce.php">Poser une annonce</a></td>
-				<td><a href="../compte/mes_annonces.php">Mes annonces</a></td>
-			</table>
-		</nav>
-	</header>
+		</form>
+	</div>
+	<section>		
+		<?php
+		if(isset($_SESSION['pseudo']))
+		{
+			include("../invariants/connexion_connecte.php");
+		}
+		else
+		{
+			include("../invariants/connexion_non_connecte.php");
+		}
+		?>
+	</section>
+	<br/>
+	<br/>
+	<nav id="navigation">
+		<table>
+			<td><a href="../offres/offres.php">Offres</a></td>
+			<td><a href="../formulaires/annonce.php">Poser une annonce</a></td>
+			<td><a href="../compte/mes_annonces.php">Mes annonces</a></td>
+		</table>
+	</nav>
+</header>
