327b0612abc2fcdce9babad95d4cb347de987b14
grandmechantloup <louis.haniquaut@gmail.com>
2015-06-03 10:56:30 +0200

    louis

--- compte/mon_compte.php
+++ compte/mon_compte.php
@@ -22,31 +22,39 @@
 		{
 		?>
 			<section>
-				<h1>
-					Modification de mes informations personnelles
-				</h1>
-				<?php
-				$req=$bdd->prepare('SELECT * FROM utilisateurs WHERE id_utilisateur=?');
-				$req->execute(array($_SESSION['id_utilisateur']));
-				$donnees=$req->fetch();
-				?>
-				<form method="POST" action="../compte/traitement_modifier_info.php">
-					civilité : <select name="Civilite" id="Civilite">
-						<option value="Monsieur">Monsieur</option>
-						<option value="Madame">Madame</option>
-					</select><br/>
-					Nom : <input type="type" name="nouveau_nom" value="<?=$donnees['Nom']?>"/><br/>
-					Prénom : <input type="text" name="nouveau_prenom" value="<?=$donnees['Prenom']?>"/><br/>
-					Téléphone :	<input type="text" name="nouveau_tel" value="0<?=$donnees['Tel']?>"/><br/>
-					Date de naissance : <input type="date" name="nouvelle_naissance" value="<?=$donnees['Naissance']?>"/><br/>
-					<input type="submit" value="Valider"/>
-				</form>	
+				<a class="deco" href="deconnection" <p>Se déconnecter</p> </a>
+
+				<fieldset class="grand_cadre3">
+				<a href="../accueil/accueil.php" ><img src="../images/images_site/logo_i_need_eat.png" border="0"></a>
+					<h1>
+						Modification de mes informations personnelles
+					</h1>
+					<?php
+					$req=$bdd->prepare('SELECT * FROM utilisateurs WHERE id_utilisateur=?');
+					$req->execute(array($_SESSION['id_utilisateur']));
+					$donnees=$req->fetch();
+					?>
+					<form method="POST" action="../compte/traitement_modifier_info.php">
+						civilité : <select name="Civilite" id="Civilite">
+							<option value="Monsieur">Monsieur</option>
+							<option value="Madame">Madame</option>
+						</select><br/>
+						Nom : <input type="type" name="nouveau_nom" value="<?=$donnees['Nom']?>"/><br/>
+						Prénom : <input type="text" name="nouveau_prenom" value="<?=$donnees['Prenom']?>"/><br/>
+						Téléphone :	<input type="text" name="nouveau_tel" value="0<?=$donnees['Tel']?>"/><br/>
+						Date de naissance : <input type="date" name="nouvelle_naissance" value="<?=$donnees['Naissance']?>"/><br/>
+						<input type="submit" value="Valider"/>
+					</form>	
+				</fieldset>
 		<?php
 		}
 		elseif(isset($_GET['modifier_info']) AND $_GET['modifier_info']==2)
 		{
 		?>
-				<p>
+				<a class="deco" href="deconnection" <p>Se déconnecter</p> </a>
+
+				<fieldset class="grand_cadre2">
+				<a href="../accueil/accueil.php" ><img src="../images/images_site/logo_i_need_eat.png" border="0"></a>
 					<h3> Adresse : </h3> 
 					<?php
 					$req=$bdd->prepare('SELECT * FROM utilisateurs WHERE id_utilisateur=?');
@@ -54,13 +62,13 @@
 					$donnees=$req->fetch();
 					?>
 					<form method="POST" action="../compte/traitement_modifier_info.php">
-						Adresse : <input type="text" name="nouveau_num_rue" value="<?=$donnees['Num_rue']?>" size="3"/> <input type="text" name="nouvelle_adresse" value="<?=$donnees['Adresse']?>"/> <br/>
-						Ville : <input type="text" name="nouvelle_ville" value="<?=$donnees['Ville']?>"/><br/>
-						Region : <input type="text" name="nouvelle_region" value="<?=$donnees['Region']?>"/><br/>
-						Code postal : <input type="text" name="nouveau_code_postal" value="<?=$donnees['Code_postal']?>"/> <br/>
+						Adresse : <input type="text" name="nouveau_num_rue" value="<?=$donnees['Num_rue']?>" size="3"/> <input type="text" name="nouvelle_adresse" value="<?=$donnees['Adresse']?>" size="40"/> <br/>
+						Ville : <input type="text" name="nouvelle_ville" value="<?=$donnees['Ville']?>" size="40"/><br/>
+						Region : <input type="text" name="nouvelle_region" value="<?=$donnees['Region']?>" size="40"/><br/>
+						Code postal : <input type="text" name="nouveau_code_postal" value="<?=$donnees['Code_postal']?>" size="10"/> <br/>
 						<input type="submit" value="Valider"/>
 					</from>
-				</p>	
+				</fieldset>	
 		<?php
 		}
 		else
