06c52c095de0308bce1472d5016986242c9acf3d
MathieuGi <math.girardon@hotmail.fr>
2015-06-03 10:54:57 +0200

    qcsd
    qs

--- invariants/header.php
+++ invariants/header.php
@@ -10,17 +10,7 @@
 		<h2> Vente et échange de fruits et légumes frais </h2>
 		<form action="" method="post" >
 			<p>
-<<<<<<< Updated upstream
 				<input type="search" name="recherche" id="Barre" placeholder="Fruits, légumes, ..." size="60" maxlength="50"/>
-=======
-<<<<<<< HEAD
-				<a href="../accueil/accueil.php">
-					<img src="../images/images_site/logo_i_need_eat_miniature.png" alt="logo I need Eat" title="page d'accueil" id="logo">
-				</a>
-=======
-				<input type="search" name="recherche" id="Barre" placeholder="Fruits, légumes, ..." size="60" maxlength="50"/>
->>>>>>> origin/master
->>>>>>> Stashed changes
 			</p>
 		</form>
 	</div>
