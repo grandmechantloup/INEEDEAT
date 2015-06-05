2ee3654f13c2ccfdd17afa9c0bc7b943f0b2914d
Gatty <simongatty3@gmail.com>
2015-06-05 10:02:26 +0200

    --'

--- bdd/connexion.php
+++ bdd/connexion.php
@@ -2,7 +2,7 @@
 $bdd=NULL;
 try
 {
-	$bdd = new PDO("mysql:host=localhost;dbname=i_need_eat;charset=utf8",'root','');
+	$bdd = new PDO("mysql:host=localhost;dbname=i_need_eat;charset=utf8",'root','root');
 }
 catch (Exception $e)
 {
