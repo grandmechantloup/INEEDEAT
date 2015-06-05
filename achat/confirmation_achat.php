74a110791e2d48f1d1664d9600f8cfc04d0fd3e8
MathieuGi <math.girardon@hotmail.fr>
2015-06-05 11:12:19 +0200

    sz
    zz

---
+++ achat/confirmation_achat.php
@@ -0,0 +1,10 @@
+<?php session_start(); ?>
+<!DOCTYPE>
+<html>
+<head>
+	<title>confirmation d'achat</title>
+	<meta charset="utf-8"/>
+</head>
+<body>
+	<?php include("../invariants/header.php");
+	
