ac7424d1205f2784b9b54ab5ab5f4d765381ce03
MathieuGi <math.girardon@hotmail.fr>
2015-06-08 16:40:30 +0200

    azed
    zae

--- invariants/aide_et_service.php
+++ invariants/aide_et_service.php
@@ -22,22 +22,22 @@
 
 <h3>Vous avez un problème de connection à votre compte</h3>
 
-<h4>En cas de problème de connection, essayer de actualiser la page, si le problème persiste, essayer de changer de navigateur. Si la connection ne s'effectue toujours pas, contacter nous en cliquant <a href="nous_contacter.php">içi</a>.</h4>
+<h4>En cas de problème de connection, essayer de actualiser la page, si le problème persiste, essayer de changer de navigateur. Si la connection ne s'effectue toujours pas, contacter nous en cliquant <a href="nous_contacter.php" class="nous_contacter">içi</a>.</h4>
 
 
 
 <h3>Vous avez un problème pour afficher une page</h3>
 
-<h4>En cas de problème avec l'affichage d'une page, essayer de raffraichir la page, si le problème persiste, essayer de changer de navigateur Web, ou de le mettre à jour. Si la page est toujours inacceccible, merci de nous contacter et de nous expliquer le problème en cliquant <a href="nous_contacter.php">içi</a>.</h4>
+<h4>En cas de problème avec l'affichage d'une page, essayer de raffraichir la page, si le problème persiste, essayer de changer de navigateur Web, ou de le mettre à jour. Si la page est toujours inacceccible, merci de nous contacter et de nous expliquer le problème en cliquant <a href="nous_contacter.php" class="nous_contacter">içi</a>.</h4>
 
 
 <h3>Vous n'arrivez pas à mettre une annonce en ligne</h3>
 
-<h4>Si vous n'arrivez pas à mettre une annonce en ligne, vérifiez que les champs que vous avez remplis soient cohérent( par exemple que votre numero de telephone soit de la forme: XX XX XX XX XX ou que la photo que vous fournissez soit au bon format. Si le problème persiste, merçi de nous contacter en cliquant <a href="nous_contacter.php">içi</a>.</h4>
+<h4>Si vous n'arrivez pas à mettre une annonce en ligne, vérifiez que les champs que vous avez remplis soient cohérent( par exemple que votre numero de telephone soit de la forme: XX XX XX XX XX ou que la photo que vous fournissez soit au bon format. Si le problème persiste, merçi de nous contacter en cliquant <a href="nous_contacter.php"class="nous_contacter">içi</a>.</h4>
 
 <h3>Vous n'arrivez pas à vous mettre en relation avec un autre utilisateur</h3>
 
-<h4>Si vous n'arrivez pas à intéragir avec un autre utilisateur dans le cadre d'une transaction, assurez vous d'être connecté à votre compte. Si c'est le cas et que ça ne marche toujours pas, merçi de nous faire parvenir le problème en cliquant <a href="nous_contacter.php">içi</a>.</h4>
+<h4>Si vous n'arrivez pas à intéragir avec un autre utilisateur dans le cadre d'une transaction, assurez vous d'être connecté à votre compte. Si c'est le cas et que ça ne marche toujours pas, merçi de nous faire parvenir le problème en cliquant <a href="nous_contacter.php" class="nous_contacter">içi</a>.</h4>
 
 <h3>Autres</h3>
 
@@ -45,7 +45,7 @@
 
 
 
-<h2>Si vous avez une quelquonque remarque, une suggestion pour améliorer le site, merçi de nous écrire un message <a href="nous_contacter.php">içi</a>.</h2>
+<h2>Si vous avez une quelquonque remarque, une suggestion pour améliorer le site, merçi de nous écrire un message <a href="nous_contacter.php" class="nous_contacter">içi</a>.</h2>
 </section>
 <?php include ("../invariants/footer.php") ?>
 
